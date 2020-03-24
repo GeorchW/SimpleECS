4f23fa212ec528d5124ccf8d52decf6bd1a769f7
Georg Wicke <georg.wicke@rwth-aachen.de>
2020-03-24 17:58:29 +0100

    Initial commit

---
+++ src/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SimpleECS
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace SimpleECS.Test
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
