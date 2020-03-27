cbcc755c6647bfdd874bcdb7f33b1be5b87c36ba
Georg Wicke <georg.wicke@rwth-aachen.de>
2020-03-27 17:14:41 +0100

    Add Global attribute

--- src/Attributes.cs
+++ src/Attributes.cs
@@ -4,4 +4,8 @@ namespace SimpleECS
     sealed class BannedAttribute : System.Attribute
     {
     }
+    [System.AttributeUsage(System.AttributeTargets.Parameter, Inherited = false, AllowMultiple = false)]
+    sealed class GlobalAttribute : System.Attribute
+    {
+    }
 }
