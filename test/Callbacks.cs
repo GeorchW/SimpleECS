503ade8d213b61c5806b4abb2d0ff8bb81777a1b
Georg Wicke <georg.wicke@rwth-aachen.de>
2020-03-28 13:25:00 +0100

    Add callback tests

---
+++ test/Callbacks.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+
+namespace SimpleECS.Test
+{
+    public class Callbacks
+    {
+        struct DisposableComp
+        {
+        }
+        Scene scene;
+        Entity entity;
+        int deletesCalled = 0;
+
+        [SetUp]
+        public void Setup()
+        {
+            scene = new Scene();
+
+            entity = scene.CreateEntity();
+            entity.Add<ExampleComp1>() = new ExampleComp1 { Value = "I want to be free!" };
+            entity.Add<DisposableComp>();
+
+            scene.Callbacks.Get(typeof(DisposableComp)).ComponentRemoved += (sender, entity) => deletesCalled++;
+        }
+        
+        [Test]
+        public void OnEntityDelete()
+        {
+            Assert.That(deletesCalled, Is.EqualTo(0));
+            entity.Delete();
+            Assert.That(deletesCalled, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void OnComponentRemove()
+        {
+            Assert.That(deletesCalled, Is.EqualTo(0));
+            entity.Remove<DisposableComp>();
+            Assert.That(deletesCalled, Is.EqualTo(1));
+        }
+    }
+}
