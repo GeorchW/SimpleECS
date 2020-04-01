cad949fa80241dab4609010a6ed0523ca9af4bb4
Georg Wicke <georg.wicke@rwth-aachen.de>
2020-04-01 14:52:18 +0200

    Make attributes public

--- src/Attributes.cs
+++ src/Attributes.cs
@@ -1,15 +1,15 @@
 namespace SimpleECS
 {
     [System.AttributeUsage(System.AttributeTargets.Parameter, Inherited = false, AllowMultiple = false)]
-    sealed class BannedAttribute : System.Attribute
+    public sealed class BannedAttribute : System.Attribute
     {
     }
     [System.AttributeUsage(System.AttributeTargets.Parameter, Inherited = false, AllowMultiple = false)]
-    sealed class GlobalAttribute : System.Attribute
+    public sealed class GlobalAttribute : System.Attribute
     {
     }
     [System.AttributeUsage(System.AttributeTargets.Parameter, Inherited = false, AllowMultiple = false)]
-    sealed class ChangedAttribute : System.Attribute
+    public sealed class ChangedAttribute : System.Attribute
     {
     }
 }
